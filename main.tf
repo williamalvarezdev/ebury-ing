f7c57f33c01a2bf3b4854cf2771a9e9f6a99bd1f
William Alvarez <williamalvarezdev@gmail.com>
2025-09-02 21:32:22 +0100

    typo

--- main.tf
+++ main.tf
@@ -320,7 +320,7 @@ resource "aws_msk_cluster" "kafka" {
   }
 
   encryption_info {
-    encryption_at_rest_kms_key_arn = aws_kms_key.kms.arn
+    encryption_at_rest_kms_key_arn = aws_kms_key.msk.arn
   }
 
   tags = {
